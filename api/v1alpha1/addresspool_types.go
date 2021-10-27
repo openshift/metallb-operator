c837031b4fc190ba08b3644370a6ffca52d20f09
Mohamed Mahmoud <mmahmoud@redhat.com>
2021-10-27 10:59:23 -0400

    Remove unused addresspool name field
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- api/v1alpha1/addresspool_types.go
+++ api/v1alpha1/addresspool_types.go
@@ -22,9 +22,6 @@ import (
 
 // AddressPoolSpec defines the desired state of AddressPool
 type AddressPoolSpec struct {
-	// Address Pool Name
-	Name string `json:"name,omitempty"`
-
 	// Protocol can be used to select how the announcement is done,
 	// +kubebuilder:validation:Enum:=layer2; bgp
 	Protocol string `json:"protocol"`
