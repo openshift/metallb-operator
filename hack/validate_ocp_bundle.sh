b8065f5fa120ad4de8da8db9be65fcf60bd9c9a5
Federico Paolinelli <fpaoline@redhat.com>
2022-09-05 12:01:41 +0200

    WIP: add sleep to valdiate e2e tests
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/validate_ocp_bundle.sh
+++ hack/validate_ocp_bundle.sh
@@ -7,3 +7,6 @@ mkdir -p _cache/bundle/manifests
 cp manifests/stable/*.yaml _cache/bundle/manifests
 cp -r bundle/metadata _cache/bundle/
 operator-sdk bundle validate _cache/bundle
+
+echo "sleeping"
+sleep 3h
