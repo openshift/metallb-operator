48c334e6add2d46e3f17bbefabe53a1d06007d7d
yahlifried <yfriedla@redhat.com>
2026-05-13 14:41:51 +0300

    added new env variables to use custom label selector in test

--- hack/ocp_run_e2e.sh
+++ hack/ocp_run_e2e.sh
@@ -5,6 +5,9 @@ export OO_INSTALL_NAMESPACE=openshift-metallb-operator
 export USE_LOCAL_RESOURCES=true
 export IS_OPENSHIFT=1
 export FRRK8S_EXTERNAL_NAMESPACE="openshift-frr-k8s"
+export FRRK8S_DAEMONSET_LABEL_SELECTOR="component=frr-k8s"
+export FRRK8S_STATUSCLEANER_DEPLOYMENT_NAME="frr-k8s-statuscleaner"
+export FRRK8S_STATUSCLEANER_LABEL_SELECTOR="component=frr-k8s-statuscleaner"
 
 hack/validate_ocp_bundle.sh
 go test --tags=validationtests -v ./test/e2e/validation -ginkgo.v -junit /logs/artifacts/ -report /logs/artifacts/
