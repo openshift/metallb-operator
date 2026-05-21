3f8a04693ff44a58d426df568b47ef0935a96a00
yahlifried <yfriedla@redhat.com>
2026-05-21 10:14:37 +0300

    Openshift only: set the e2e env vars to align with CNO's deployment labels
    This change is needed because of the different way CNO deploys metallb and frr-k8s, the variables added are for the frr-k8s daemon annotation, statuscleaner deployment name, and statuscleaner annotation, which are deployed differently upstream
    
    Signed-off-by: yahlifried <yfriedla@redhat.com>

--- hack/ocp_run_e2e.sh
+++ hack/ocp_run_e2e.sh
@@ -5,7 +5,10 @@ export OO_INSTALL_NAMESPACE=openshift-metallb-operator
 export USE_LOCAL_RESOURCES=true
 export IS_OPENSHIFT=1
 export FRRK8S_EXTERNAL_NAMESPACE="openshift-frr-k8s"
+export FRRK8S_DAEMONSET_LABEL_SELECTOR="component=frr-k8s"
+export FRRK8S_STATUSCLEANER_DEPLOYMENT_NAME="frr-k8s-statuscleaner"
+export FRRK8S_STATUSCLEANER_LABEL_SELECTOR="component=frr-k8s-statuscleaner"
 
 hack/validate_ocp_bundle.sh
 go test --tags=validationtests -v ./test/e2e/validation -ginkgo.v -junit /logs/artifacts/ -report /logs/artifacts/
-go test --tags=e2etests -v ./test/e2e/functional -ginkgo.v -ginkgo.skip "with BGP type" -junit /logs/artifacts/ -report /logs/artifacts/
+go test --tags=e2etests -v ./test/e2e/functional -ginkgo.v -junit /logs/artifacts/ -report /logs/artifacts/
